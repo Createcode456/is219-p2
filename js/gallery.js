5b638f04a45c14b0d9def407d79fdc0b85fa6276
Createcode456 <225afarber@frhsd.com>
2025-01-14 09:26:19 -0500

    please work

--- js/gallery.js
+++ js/gallery.js
@@ -63,7 +63,6 @@ var mImages = [];
 
 // Holds the retrived JSON information
 var mJson;
-
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 var mUrl;
